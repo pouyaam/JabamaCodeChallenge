5a682b9d94640922c82f1287904c77856fe05df6
Hocein <hosein.kordloo@gmail.com>
2024-08-15 15:53:32 +0330

    Improve RetryComponent functionality

--- app/src/main/java/com/jabama/challenge/core/ui/components/RetryComponent.kt
+++ app/src/main/java/com/jabama/challenge/core/ui/components/RetryComponent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.jabama.challenge.core.designsystem.theme.JabamaTheme
 import com.jabama.challenge.github.R
@@ -18,19 +21,27 @@ import com.jabama.challenge.github.R
 @Composable
 fun RetryComponent(
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    text: String = stringResource(id = R.string.general_error),
+    textButton: String = stringResource(id = R.string.retry),
 ) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.global_error))
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(horizontal = JabamaTheme.paddings.p16)
+                .fillMaxWidth()
+        )
 
         Spacer(modifier = Modifier.size(JabamaTheme.sizes.s32))
 
         Button(onClick = onClick) {
-            Text(text = stringResource(R.string.retry))
+            Text(text = textButton)
         }
     }
 }
