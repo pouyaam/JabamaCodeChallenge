56b2be2c1f97f29a5db268143808e482f281ba5e
Hocein <hosein.kordloo@gmail.com>
2024-08-15 12:55:19 +0330

    Add global models such as GlobalError and Result

---
+++ app/src/main/java/com/jabama/challenge/core/common/model/GeneralError.kt
@@ -0,0 +1,7 @@
+package com.jabama.challenge.core.common.model
+
+sealed class GeneralError {
+  data class ApiError(val message: String?, val code: Int) : GeneralError()
+  data object NetworkError : GeneralError()
+  data class UnknownError(val error: Throwable) : GeneralError()
+}
