5b9782c134d7203c73243f7305a163f0fe4e8784
r.zarchi <r.zarchi@arissystem.com>
2024-09-10 12:06:04 +0330

    Moved app modules to a file and clean unused objects

--- app/src/main/java/com/jabama/challenge/app/App.kt
+++ app/src/main/java/com/jabama/challenge/app/App.kt
@@ -1,15 +1,12 @@
 package com.jabama.challenge.app
 
 import android.app.Application
-import androidx.preference.PreferenceManager
 import com.jabama.challenge.common.di.networkModule
-import com.jabama.challenge.network.di.accessTokenModule
-import com.jabama.challenge.repository.token.TokenRepositoryImpl
+import com.jabama.challenge.di.appModule
+import com.jabama.challenge.token.di.accessTokenModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.core.qualifier.named
-import org.koin.dsl.module
-const val APPLICATION_CONTEXT = "APPLICATION_CONTEXT"
+
 class App : Application() {
 
     override fun onCreate() {
@@ -18,12 +15,6 @@ class App : Application() {
             androidContext(this@App)
             modules(listOf(appModule, networkModule, accessTokenModule))
         }
-    }
-
-    val appModule = module {
-        factory { TokenRepositoryImpl(get()) }
-        single(named(APPLICATION_CONTEXT)) { applicationContext }
-        single { PreferenceManager.getDefaultSharedPreferences(get()) }
     }
 
 }
